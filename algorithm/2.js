805b860b62b5962ce4e6effa889cae66a0052a51
arsame-th <shizennohanashi@gmail.com>
2021-09-19 01:57:42 +0700

    fix algo 02

--- algorithm/2.js
+++ algorithm/2.js
@@ -15,20 +15,17 @@ rl.question('distribute input: ', async (input) => {
 function distribute(input) {
     const inputs = input.split(',');
     var output_arr = []
-    var server_min_job = 0 ;
     var server_min_job_index = 0 ;
     for (let i=0;i<inputs[0];i++) {   
         output_arr.push([])
     } 
      
-    for (let job=0;job<inputs[1];job++){ 
-        for (let i=0;i<output_arr.length;i++) {   
-            if(output_arr[i].length <= server_min_job) {
-                server_min_job = output_arr[i].length
-                server_min_job_index = i
-            }
-        }     
+    for (let job=0;job<inputs[1];job++){  
+        if(server_min_job_index >= output_arr.length){
+            server_min_job_index = 0
+        }
         output_arr[server_min_job_index].push(job)
+        server_min_job_index++;
     }
     return output_arr
 }
